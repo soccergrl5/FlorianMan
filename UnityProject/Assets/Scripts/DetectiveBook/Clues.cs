5ce9f1f96c79cb81763fd6515a6a0caa4d9eb99f
soccergrl5 <101655846+soccergrl5@users.noreply.github.com>
2026-06-21 23:57:41 +0200

    Clues Enum

---
+++ UnityProject/Assets/Scripts/DetectiveBook/Clues.cs
@@ -0,0 +1,29 @@
+﻿namespace FlorianMan.DetectiveBook
+{
+    public enum Clues
+    {
+        RavioliCan,
+        FiveBananasMorning,
+        FourBananasAfter,
+        BananaPeelBehindChair,
+        BananaPeelNextToCorpse,
+        
+        PartyInvitation,
+        CakeRecipe,
+        ButterFreezeInFridge,
+        ButterNextToCircularSaw,
+        ChopedOfFinger,
+        MissingFingerOnCorpse,
+        
+        EmergencyNoteUnderShelf,
+        EmergencyNoteInBedRoom,
+        
+        CeilingFanSwitchOnNormal,
+        CeilingFanSwitchOnUltra,
+        CeilingFanWorking,
+        CeilingFanBroken,
+        
+        ThrowingStarsInCorps,
+        ThrowingStarsHangingOnCeiling,
+    }
+}
